0d9b61ca60fbd51487ca8a287633c9c13f6aa051
Djumabaevs <djumabaevb@gmail.com>
2021-06-29 12:02:11 +0600

    add base url and generic fun to build api with retrofit

--- app/src/main/java/com/bignerdranch/android/loginauthkotlin/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/loginauthkotlin/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.bignerdranch.android.loginauthkotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bignerdranch.android.loginauthkotlin.ui.auth.AuthActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        finish()
+        startActivity(Intent(this, AuthActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/bignerdranch/android/loginauthkotlin/ui/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.bignerdranch.android.loginauthkotlin.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bignerdranch.android.loginauthkotlin.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
