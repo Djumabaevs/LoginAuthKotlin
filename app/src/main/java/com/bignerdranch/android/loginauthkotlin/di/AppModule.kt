2b81ac82587a03a95bf9dd2c1d7ada251b51f8eb
Djumabaevs <djumabaevb@gmail.com>
2021-06-30 00:24:47 +0600

    @Singleton and @Provides

--- app/src/main/java/com/bignerdranch/android/loginauthkotlin/di/AppModule.kt
+++ app/src/main/java/com/bignerdranch/android/loginauthkotlin/di/AppModule.kt
@@ -1,12 +1,33 @@
 package com.bignerdranch.android.loginauthkotlin.di
 
+import android.content.Context
+import com.bignerdranch.android.loginauthkotlin.data.network.AuthApi
+import com.bignerdranch.android.loginauthkotlin.data.network.RemoteDataSource
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
 
 @Module
 @InstallIn(ApplicationComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRemoteDataSource(): RemoteDataSource {
+        return RemoteDataSource()
+    }
+
+    @Singleton
+    @Provides
+    fun provideAuthApi(
+        remoteDataSource: RemoteDataSource,
+        @ApplicationContext context: Context
+    ): AuthApi {
+        return remoteDataSource.buildApi(AuthApi::class.java, context)
+    }
 
 
 
