3736b73d44f17c816c84225438228f7af0709e0c
Djumabaevs <djumabaevb@gmail.com>
2021-06-29 15:55:16 +0600

    done with home and user

--- app/src/main/java/com/bignerdranch/android/loginauthkotlin/ui/base/ViewModelFactory.kt
+++ app/src/main/java/com/bignerdranch/android/loginauthkotlin/ui/base/ViewModelFactory.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.bignerdranch.android.loginauthkotlin.data.repository.AuthRepository
 import com.bignerdranch.android.loginauthkotlin.data.repository.BaseRepository
+import com.bignerdranch.android.loginauthkotlin.data.repository.UserRepository
 import com.bignerdranch.android.loginauthkotlin.ui.auth.AuthViewModel
+import com.bignerdranch.android.loginauthkotlin.ui.home.HomeViewModel
 import java.lang.IllegalArgumentException
 
 class ViewModelFactory(
@@ -13,8 +15,11 @@ class ViewModelFactory(
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
             return  when{
-            modelClass.isAssignableFrom(AuthViewModel::class.java) ->
-                AuthViewModel(repository as AuthRepository) as T
+                modelClass.isAssignableFrom(AuthViewModel::class.java) ->
+                    AuthViewModel(repository as AuthRepository) as T
+
+                modelClass.isAssignableFrom(HomeViewModel::class.java) ->
+                    HomeViewModel(repository as UserRepository) as T
             else -> throw IllegalArgumentException("ViewModel class not found")
         }
     }
--- app/src/main/java/com/bignerdranch/android/loginauthkotlin/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/loginauthkotlin/ui/home/HomeFragment.kt
@@ -5,16 +5,66 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.bignerdranch.android.loginauthkotlin.R
+import com.bignerdranch.android.loginauthkotlin.data.network.Resource
+import com.bignerdranch.android.loginauthkotlin.data.network.UserApi
+import com.bignerdranch.android.loginauthkotlin.data.repository.UserRepository
+import com.bignerdranch.android.loginauthkotlin.data.responses.User
+import com.bignerdranch.android.loginauthkotlin.databinding.FragmentHomeBinding
+import com.bignerdranch.android.loginauthkotlin.ui.base.BaseFragment
+import com.bignerdranch.android.loginauthkotlin.ui.visible
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment<HomeViewModel, FragmentHomeBinding, UserRepository>() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
+        binding.progressbar.visible(false)
+
+        viewModel.getUser()
+
+        viewModel.user.observe(viewLifecycleOwner, Observer {
+            when(it) {
+                is Resource.Success -> {
+                    binding.progressbar.visible(false)
+                    updateUI(it.value.user)
+                }
+                is Resource.Loading -> {
+                    binding.progressbar.visible(true)
+                }
+                is Resource.Failure -> {
+//                    handleApiError(it)
+                }
+            }
+        })
     }
+
+    private fun updateUI(user: User) {
+        with(binding) {
+            textViewId.text = user.id.toString()
+            textViewName.text = user.name
+            textViewEmail.text = user.email
+        }
+    }
+
+
+    override fun getViewModel() = HomeViewModel::class.java
+
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) = FragmentHomeBinding.inflate(inflater, container, false)
+
+    override fun getFragmentRepository(): UserRepository {
+        val token = runBlocking { userPreferences.authToken.first() }
+        val api = remoteDataSource.buildApi(UserApi::class.java, token)
+        return UserRepository(api)
+    }
+
+
 }
